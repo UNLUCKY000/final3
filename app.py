623de421dbea2e1434e30acc77660e8b8780f31b
UNLUCKY000 <62801726+UNLUCKY000@users.noreply.github.com>
2024-01-04 06:28:51 +0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,35 @@
+import openai
+import streamlit as st
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+
+st.title("💬 Chatbot") 
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    openai.api_key = openai_api_key
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message
+    st.session_state.messages.append(msg)
+    text = msg.content
+
+    embeddings_dataset = st.session_state.cache['EMBED']
+    synthesiser = st.session_state.cache['AUDIO_PIPE']
+    speaker_embedding = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
+    
+    speech = synthesiser(text, forward_params={"speaker_embeddings": speaker_embedding})
+    # st.write(speech)
+    st.audio(speech['audio'], sample_rate = speech['sampling_rate'])
